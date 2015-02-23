6df5ec6a278aae35e0053e8244f90eb2e4eebb72
Declan Hanley <decy.hanley@gmail.com>
2015-02-23 10:00:00 +0000

    test

--- 02_Ionic/assets/js/services.js
+++ 02_Ionic/assets/js/services.js
@@ -8,27 +8,17 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    face: 'http://www.nerdist.com/wp-content/uploads/2011/10/nerdistpodcastv2.png'
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    face: 'http://i1.sndcdn.com/avatars-000055524562-w0m7rv-original.jpg'
   }, {
     id: 2,
     name: 'Andrew Jostlin',
     lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    face: 'http://a3.mzstatic.com/us/r30/Podcasts5/v4/6a/a9/cc/6aa9cc95-a389-f5ad-1ab1-5ac5b8265363/mza_3638591931975247127.600x600-75.jpg'
   }];
 
   return {
