cc36535617635b9c7afb4f3984932816b693951e
Declan Hanley <decy.hanley@gmail.com>
2015-02-23 06:23:24 +0000

    Please...

--- 02_Ionic_App/js/services.js
+++ 02_Ionic_App/js/services.js
@@ -7,29 +7,19 @@ angular.module('starter.services', [])
   var chats = [{
     id: 0,
     name: 'Podcast 001',
-    lastText: 'What Will They Say this week',
+    lastText: 'Hello World',
     face: 'http://www.berksarl.org/wp-content/uploads/2013/10/podcast.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    name: 'Podcast 152',
+    lastText: 'They Talk about Podcast Apps',
+    face: 'http://www.berksarl.org/wp-content/uploads/2013/10/podcast.jpg'
   }, {
     id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, /*{
-    id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];*/
+    name: 'Podcast 005',
+    lastText: 'We\'re new don\'t be hating',
+    face: 'http://www.berksarl.org/wp-content/uploads/2013/10/podcast.jpg'
+  }];
 
   return {
     all: function() {
@@ -48,49 +38,3 @@ angular.module('starter.services', [])
     }
   }
 })
-
-/**
- * A simple example service that returns some data.
- */
-/*.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var friends = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    notes: 'Enjoys drawing things',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    notes: 'Odd obsession with everything',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlen',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    notes: 'I think he needs to buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    notes: 'Just the nicest guy',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
-
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-});*/
