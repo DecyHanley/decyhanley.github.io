04cd796ffe28d29966615e1a71fb389785eba5a7
Declan Hanley <decy.hanley@gmail.com>
2015-02-23 05:41:56 +0000

    Test 3

--- 02_Ionic_App/js/services.js
+++ 02_Ionic_App/js/services.js
@@ -7,27 +7,27 @@ angular.module('starter.services', [])
   var chats = [{
     id: 0,
     name: 'Ben Sparrow',
-    lastText: 'You on your way?',
+    //lastText: 'You on your way?',
     face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
   }, {
     id: 1,
     name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
+    //lastText: 'Hey, it\'s me',
     face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
   }, {
     id: 2,
     name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
+    //lastText: 'Did you get the ice cream?',
     face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
   }, {
     id: 3,
     name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
+    //lastText: 'I should buy a boat',
     face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
   }, {
     id: 4,
     name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
+    //lastText: 'Look at my mukluks!',
     face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
   }];
 
