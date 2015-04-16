11643d716fbca090164e47be0233e3bade4d693c
DecyHanley <decy.hanley@gmail.com>
2015-04-16 14:11:22 +0000

    Added ajax

--- coolApp/js/custom.js
+++ coolApp/js/custom.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
   
   $('button#your-name-button').click(function() {
+    
     console.log("your-name-button clicked");
     
+    $.ajax({
+      method: "GET",
+      url: "//immense-harbor-7957.herokuapp.com"
+    }).done(function(response) {
+      
+    }).fail(function(mgs){
+      
+    });
+    
   });
+  
 });
