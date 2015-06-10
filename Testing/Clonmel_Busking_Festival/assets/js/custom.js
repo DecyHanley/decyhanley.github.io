61a0253ccd209cae69f96c7ae3fcc6a98be8c704
Declan Hanley <decy.hanley@gmail.com>
2015-06-10 16:34:40 +0100

    Restructuring site

---
+++ Testing/Clonmel_Busking_Festival/assets/js/custom.js
@@ -0,0 +1,51 @@
+// Closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+//Carousel
+$(document).ready(function(){
+     $('#myCarousel').carousel({
+       interval: 2500
+     });
+ });
+
+//Back to top Button
+$(document).ready(function(){
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  //Click event to scroll to top
+  $('#back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1000);
+    return false;
+  });
+});
