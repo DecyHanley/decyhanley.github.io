56ff7fe92cfbdf713d6cf4d5663ffb5cdad7b236
Declan Hanley <decy.hanley@gmail.com>
2015-06-18 03:07:39 +0100

    Few changes

--- Testing/Clonmel_Busking_Festival/assets/js/custom.js
+++ Testing/Clonmel_Busking_Festival/assets/js/custom.js
@@ -1,67 +1,41 @@
-$(document).ready(function()  {
-    // Closes the sidebar menu
-    $("#menu-close").click(function(e)  {
-      e.preventDefault();
-      $("#sidebar-wrapper").toggleClass("active");
-    });
-    // Opens the sidebar menu
-    $("#menu-toggle").click(function(e) {
-      e.preventDefault();
-      $("#sidebar-wrapper").toggleClass("active");
-    });
-    // Scrolls to the selected menu item on the page
-    $(function()  {
-      $('a[href*=#]:not([href=#])').click(function()  {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length)  {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
-
-    //Carousel
-    $('#myCarousel').carousel(  {
-      interval: 2500
-    });
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#myCarousel").carousel(2);
-    });
-    $(".item4").click(function(){
-        $("#myCarousel").carousel(3);
-    });
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $("#myCarousel").carousel("prev");
-    });
-    $(".right").click(function(){
-        $("#myCarousel").carousel("next");
-    });
-
-    //Back to top Button
-    //Check to see if the window is top if not then display button
-    $(window).scroll(function(){
-      if ($(this).scrollTop() > 100) {
-        $('#back-to-top').fadeIn();
-      } else {
-        $('#back-to-top').fadeOut();
-      }
-    });
-    //Click event to scroll to top
-    $('#back-to-top').click(function(){
-      $('html, body').animate({scrollTop : 0},1000);
-      return false;
-    });
+$(document).ready(function() {
+	$("#menu-close").click(function(e) {
+		e.preventDefault();
+		$("#sidebar-wrapper").toggleClass("active");
+	});
+	$("#menu-toggle").click(function(e) {
+		e.preventDefault();
+		$("#sidebar-wrapper").toggleClass("active");
+	});
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(
+				/^\//, '') || location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +
+					']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+	//Back to top Button
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+			$('#back-to-top').fadeIn();
+		}
+		else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+	$('#back-to-top').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
+		return false;
+	});
 });
