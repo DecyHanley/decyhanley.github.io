b5af3332c6be76663c67465c8b477f6b4c77f5d9
Declan Hanley <decy.hanley@gmail.com>
2015-07-24 18:57:17 +0100

    api updates

--- clonmel-busking-festival/assets/js/facebook.js
+++ clonmel-busking-festival/assets/js/facebook.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: 'your-app-id',
+      xfbml: true,
+      version: 'v2.4'
+    });
+  };
+
   (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
+    if (d.getElementById(id)) {
+      return;
+    }
     js = d.createElement(s);
     js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+    js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 });
