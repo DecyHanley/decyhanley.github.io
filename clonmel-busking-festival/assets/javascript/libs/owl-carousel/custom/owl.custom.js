f346c5e04019294057107bd1c42e9574b4c1be83
Declan Hanley <decy.hanley@gmail.com>
2015-07-30 16:25:53 +0100

    big changes

---
+++ clonmel-busking-festival/assets/javascript/libs/owl-carousel/custom/owl.custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function()	{
+  //Owl Carousel
+  $("#music").owlCarousel({
+    singleItem: true,
+    navigation : true,
+    navigationText: [
+    "<i class='fa  fa-1x fa-arrow-left'></i>",
+    "<i class='fa  fa-1x fa-arrow-right'></i>"
+    ],
+    slideSpeed : 200,
+    paginationSpeed : 800,
+  });
+  $("#photo").owlCarousel({
+    singleItem: true,
+    navigation : true,
+    navigationText: [
+    "<i class='fa  fa-1x fa-arrow-left'></i>",
+    "<i class='fa  fa-1x fa-arrow-right'></i>"
+    ],
+    autoPlay: 2500,
+    slideSpeed : 200,
+    paginationSpeed : 800,
+  });
+});
