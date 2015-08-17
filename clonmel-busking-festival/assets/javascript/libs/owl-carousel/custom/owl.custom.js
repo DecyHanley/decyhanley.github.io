d92d19abf2170ef069d2461f8dfa898a19d079b1
Declan Hanley <decy.hanley@gmail.com>
2015-08-17 18:36:58 +0100

    test

--- clonmel-busking-festival/assets/javascript/libs/owl-carousel/custom/owl.custom.js
+++ clonmel-busking-festival/assets/javascript/libs/owl-carousel/custom/owl.custom.js
@@ -3,13 +3,29 @@ $(document).ready(function() {
   $("#podcasts").owlCarousel({
     singleItem: true,
     navigation: true,
+    pagination : false,
     navigationText: [
       "<i class='fa  fa-1x fa-arrow-left'></i>",
       "<i class='fa  fa-1x fa-arrow-right'></i>"
     ],
     slideSpeed: 200,
     paginationSpeed: 800,
+    jsonPath: "assets/javascript/libs/owl-carousel/custom/podcasts.json",
+    jsonSuccess: customDataSuccess
   });
+
+  function customDataSuccess(data) {
+    var content = "";
+    for (var i in data["items"]) {
+      var scrolling = data["items"][i].scrolling;
+      var frameborder = data["items"][i].frameborder;
+      var src = data["items"][i].src;
+
+      content += "<iframe scrolling=\"" + scrolling + "\" frameborder=\"" + frameborder + "\" src=\"" + src + "\">"
+    }
+    $("#podcasts").html(content);
+  }
+
 
   $("#photos").owlCarousel({
     items: 2,
