ee85db96150eed3df15b12e1818db75c2a96c76c
Declan Hanley <decy.hanley@gmail.com>
2015-08-17 13:03:25 +0100

    test

--- clonmel-busking-festival/assets/javascript/libs/owl-carousel/custom/owl.custom.js
+++ clonmel-busking-festival/assets/javascript/libs/owl-carousel/custom/owl.custom.js
@@ -10,6 +10,24 @@ $(document).ready(function() {
     slideSpeed: 200,
     paginationSpeed: 800,
   });
+
+  $("#photos").owlCarousel({
+    jsonPath: "assets/javascript/libs/owl-carousel/custom/owl.custom.json",
+    jsonSuccess: customDataSuccess
+  });
+
+  function customDataSuccess(data) {
+    var content = "";
+    for (var i in data["items"]) {
+
+      var img = data["items"][i].img;
+      var alt = data["items"][i].alt;
+      var caption = data["items"][i].caption;
+
+      content += "<img src=\"" + img + "\" alt=\"" + alt + "\" caption=\"" + caption + "\">"
+    }
+    $("#photos").html(content);
+  }
   $("#photos").owlCarousel({
     singleItem: true,
     navigation: true,
