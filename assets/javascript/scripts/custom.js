54fd4ddc416f4f87c48b59f2d52a305def4af69a
Declan Hanley <decy.hanley@gmail.com>
2015-08-28 02:08:01 +0100

    fixed some stuff on my personal site

---
+++ assets/javascript/scripts/custom.js
@@ -0,0 +1,15 @@
+/*------------------------------------------------------------------------------
+	Custom Javascript
+------------------------------------------------------------------------------*/
+$(document).ready(function() {
+
+	//Cover for Pace
+	$(window).load(function() {
+		$(".cover").fadeOut(2500);
+	});
+
+	//Nice Scroll
+	$(window).load(function()	{
+		$("html").niceScroll();
+	});
+});
